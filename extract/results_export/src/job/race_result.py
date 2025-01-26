0d69b702f6180fbfe60f15884c46f9cf3e8ace83
LucLagrange <128151278+LucLagrange@users.noreply.github.com>
2025-01-26 12:48:14 +0100

    Airbyte Manifest with streams and python script for results (#1)
    * Airbyte Manifest with streams and python script for results
    
    * Dockerization
    
    * Increased wait time
    
    * chained docker commands
    
    * better xait time
    
    * Fixed at one year (2024)
    
    * Season year as docker env variable
    
    * Changed season year to have 2023 data

---
+++ extract/results_export/src/job/race_result.py
@@ -0,0 +1,116 @@
+import requests
+import logging
+import time
+import os
+from typing import Dict, List, Optional
+from google.cloud import bigquery
+
+# Configure the logging module
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# Function to fetch F1 race results
+def fetch_f1_results(season: str, round_number: str) -> Optional[Dict]:
+    base_url = "http://api.jolpi.ca/ergast/f1"
+    url = f"{base_url}/{season}/{round_number}/results"
+    logging.info(
+        "Fetching race results for Season: %s, Round: %s", season, round_number
+    )
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        logging.info("Successfully fetched race results")
+        return data
+    except requests.exceptions.RequestException as e:
+        logging.error("Error fetching the race results: %s", e)
+        return None
+
+
+# Function to process and format race results
+def process_race_results(data: Dict) -> List[Dict]:
+    logging.info("Processing race results data")
+    processed_data: List[Dict] = []
+
+    if (
+        "MRData" in data
+        and "RaceTable" in data["MRData"]
+        and "Races" in data["MRData"]["RaceTable"]
+    ):
+        races = data["MRData"]["RaceTable"]["Races"]
+        for race in races:
+            race_info = {
+                "season": race["season"],
+                "round": race["round"],
+            }
+            for result in race["Results"]:
+                driver_result = race_info.copy()
+                driver_result.update(
+                    {
+                        "position": result["position"],
+                        "points": result["points"],
+                        "driver_id": result["Driver"]["driverId"],
+                        "constructor_id": result["Constructor"]["constructorId"],
+                        "grid": result["grid"],
+                        "laps": result["laps"],
+                        "status": result["status"],
+                    }
+                )
+                processed_data.append(driver_result)
+    return processed_data
+
+
+# Function to insert data into BigQuery
+def insert_results_to_bigquery(results: List[Dict], table_id: str) -> None:
+    client = bigquery.Client()
+    logging.info("Inserting results into BigQuery")
+
+    try:
+        errors = client.insert_rows_json(table_id, results)
+        if errors:
+            logging.error(
+                "Errors occurred while inserting data into BigQuery: %s", errors
+            )
+        else:
+            logging.info("Successfully inserted data into BigQuery")
+    except Exception as e:
+        logging.error("An error occurred while inserting data into BigQuery: %s", e)
+
+
+def main() -> None:
+    # Get the table ID from environment variable
+    table_id = os.getenv("RESULTS_TABLE_ID")
+    season = os.getenv("SEASON")
+
+    if not table_id:
+        logging.error("Environment variable TABLE_ID is not set.")
+        return
+
+    rounds = [str(round) for round in range(1, 30)]  # Creates a list of rounds
+
+    for round_number in rounds:
+        data = fetch_f1_results(season, round_number)
+        # Check if data exists for the round
+        if (
+            data
+            and "MRData" in data
+            and "RaceTable" in data["MRData"]
+            and data["MRData"]["RaceTable"]["Races"]
+        ):
+            results = process_race_results(data)
+            insert_results_to_bigquery(results, table_id)
+        else:
+            logging.info(
+                "No data available for Season: %s, Round: %s. Skipping.",
+                season,
+                round_number,
+            )
+
+        # Add delay to respect rate limits
+        time.sleep(1)  # 1 seconds delay for burst limit (4 requests per second)
+
+
+if __name__ == "__main__":
+    main()
---
+++ extract/results_export/src/job/sprint_result.py
@@ -0,0 +1,112 @@
+import requests
+import logging
+import time
+import os
+from typing import Dict, List, Optional
+from google.cloud import bigquery
+
+# Configure the logging module
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# Function to fetch F1 sprint results
+def fetch_f1_sprint_results(season: str, round_number: str) -> Optional[Dict]:
+    base_url = "http://ergast.com/api/f1"
+    url = f"{base_url}/{season}/{round_number}/sprint.json"
+    logging.info(
+        "Fetching sprint results for Season: %s, Round: %s", season, round_number
+    )
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        logging.info("Successfully fetched sprint results")
+        return data
+    except requests.exceptions.RequestException as e:
+        logging.error("Error fetching the sprint results: %s", e)
+        return None
+
+
+# Function to process and format sprint results
+def process_sprint_results(data: Dict) -> List[Dict]:
+    logging.info("Processing sprint results data")
+    processed_data: List[Dict] = []
+
+    if (
+        "MRData" in data
+        and "RaceTable" in data["MRData"]
+        and "Races" in data["MRData"]["RaceTable"]
+    ):
+        races = data["MRData"]["RaceTable"]["Races"]
+        for race in races:
+            race_info = {
+                "season": race["season"],
+                "round": race["round"],
+            }
+            if "SprintResults" in race:
+                for result in race["SprintResults"]:
+                    driver_result = race_info.copy()
+                    driver_result.update(
+                        {
+                            "position": result["position"],
+                            "points": result["points"],
+                            "driver_id": result["Driver"]["driverId"],
+                            "constructor_id": result["Constructor"]["constructorId"],
+                            "grid": result["grid"],
+                            "laps": result["laps"],
+                            "status": result["status"],
+                            "event_type": "sprint",
+                        }
+                    )
+                    processed_data.append(driver_result)
+    return processed_data
+
+
+# Function to insert data into BigQuery
+def insert_results_to_bigquery(results: List[Dict], table_id: str) -> None:
+    client = bigquery.Client()
+    logging.info("Inserting sprint results into BigQuery")
+
+    try:
+        errors = client.insert_rows_json(table_id, results)
+        if errors:
+            logging.error(
+                "Errors occurred while inserting data into BigQuery: %s", errors
+            )
+        else:
+            logging.info("Successfully inserted sprint data into BigQuery")
+    except Exception as e:
+        logging.error("An error occurred while inserting data into BigQuery: %s", e)
+
+
+def main() -> None:
+    # Get the table ID from environment variable
+    table_id = os.getenv("SPRINT_TABLE_ID")
+    season = os.getenv("SEASON")
+
+    if not table_id:
+        logging.error("Environment variable SPRINT_RESULTS_TABLE_ID is not set.")
+        return
+
+    rounds = [str(round) for round in range(1, 30)]  # Example round range
+
+    for round_number in rounds:
+        sprint_data = fetch_f1_sprint_results(season, round_number)
+        if sprint_data:
+            sprint_results = process_sprint_results(sprint_data)
+            if sprint_results:
+                insert_results_to_bigquery(sprint_results, table_id)
+            else:
+                logging.info(
+                    "No sprint results to insert for Season: %s, Round: %s",
+                    season,
+                    round_number,
+                )
+
+            time.sleep(1)  # 1 seconds delay for burst limit (4 requests per second) and
+
+
+if __name__ == "__main__":
+    main()
